080b68ff081cded117e58dd7d88d77b2f4a407a9
TOMAS ESTEBAN BRICEÑO BERNAL <110701704+TEBB27@users.noreply.github.com>
2024-03-05 22:42:22 -0500

    cambio imagenes

--- src/components/FloatingButton.tsx
+++ src/components/FloatingButton.tsx
@@ -6,12 +6,12 @@ import { FloatingWhatsApp } from 'react-floating-whatsapp'
 const FloatingButton: React.FC = () => {
     return (
       <FloatingWhatsApp
-        phoneNumber={"+573504746586"}
+        phoneNumber={"+573214607611"}
         accountName={'Alejo Parrilla'}
         allowEsc
         chatMessage="¿Te apetece un buen asado? En Alejo Parrilla tenemos las carnes más jugosas y tiernas, preparadas al estilo tradicional. Haz tu pedido ahora y recíbelo en tu casa en minutos. ¡Te encantará! 🍽️🚚"
         className="custom-class-name"
-        avatar="./images/alejo-parrilla-1.jpg"
+        avatar="./images/whatsapp/whatsapp-profile.webp"
         statusMessage="Siempre a tu disposición"
         darkMode={false}
         notification={true}
--- src/components/VisonCard.tsx
+++ src/components/VisonCard.tsx
@@ -25,10 +25,10 @@ const VisionCard: React.FC<Vision> = ({ imageUrl, text, title }) => {
 const VisionComponent = () => {
   return(
     <div className="container-vision">
-      <VisionCard imageUrl="./images/alejo-parrilla-1.jpg" title="Destino culinario de referencia" text='En Alejo Parrilla, aspiramos a ser el destino culinario de referencia, liderando la industria gastronómica con inovación y tradición en cada plato. '/>
-      <VisionCard imageUrl="./images/alejo-parrilla-1.jpg" title="Punto de encuentro" text='Visualizamos nuestro restaurante como un punto de encuentro donde la excelencia de la parrilla se fusiona con la autenticidad de la cocina colombiana'/>
-      <VisionCard imageUrl="./images/alejo-parrilla-1.jpg" title="Experencia única" text='Buscamos marcar tendencias y crear recuerdos inolvidables para nuestros clientes, ofreciendo una experiencia única que refleje la autenticidad y la diversidad de nuestra oferta. '/>
-      <VisionCard imageUrl="./images/alejo-parrilla-1.jpg" title="Calidad ante todo " text='Nos esforzamos por ser reconocidos como Alejo Parrilla, un lugar donde la calidad de la parrilla y la calidez de la experiencia  se encuentran por deleitar a todos nuestos comensales.'/>
+      <VisionCard imageUrl="./images/vision/destino-culinario-de-referencia.webp" title="Destino culinario de referencia" text='En Alejo Parrilla, aspiramos a ser el destino culinario de referencia, liderando la industria gastronómica con inovación y tradición en cada plato. '/>
+      <VisionCard imageUrl="./images/vision/punto-de-encuentro.webp" title="Punto de encuentro" text='Visualizamos nuestro restaurante como un punto de encuentro donde la excelencia de la parrilla se fusiona con la autenticidad de la cocina colombiana'/>
+      <VisionCard imageUrl="./images/vision/experiencia-unica.webp" title="Experencia única" text='Buscamos marcar tendencias y crear recuerdos inolvidables para nuestros clientes, ofreciendo una experiencia única que refleje la autenticidad y la diversidad de nuestra oferta. '/>
+      <VisionCard imageUrl="./images/vision/calidad-ante-todo.webp" title="Calidad ante todo " text='Nos esforzamos por ser reconocidos como Alejo Parrilla, un lugar donde la calidad de la parrilla y la calidez de la experiencia  se encuentran por deleitar a todos nuestos comensales.'/>
     </div>
   )
 }
