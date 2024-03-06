2e4468e33042607ca64b208fac8538f25ff7ed32
TOMAS ESTEBAN BRICEÑO BERNAL <110701704+TEBB27@users.noreply.github.com>
2024-03-06 00:22:59 -0500

    cambio tamaño de fuente vision

--- src/components/loadingPage.tsx
+++ src/components/loadingPage.tsx
@@ -3,24 +3,8 @@ import './Styles.css'
 const LoadingPage = () => {
   return (
     <div className='container_spinner'>
-    <div className="spinner"></div>
+      <div className="spinner"></div>
     </div>
-    // <div className='loading page container'>
-    //   <div className="loader">
-    //       <div className="loader_item"></div>
-    //       <div className="loader_item"></div>
-    //       <div className="loader_item"></div>
-    //       <div className="loader_item"></div>
-    //       <div className="loader_item"></div> 
-    //       <div className="loader_item"></div>
-    //       <div className="loader_item"></div>
-    //       <div className="loader_item"></div>
-    //       <div className="loader_item"></div>
-    //       <div className="loader_item"></div>
-    //       <div className="loader_item"></div>
-    //       <div className="loader_item"></div>
-    //   </div>
-    // </div>
   );
 };
 
