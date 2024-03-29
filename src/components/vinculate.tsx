f35026cefe671c5669511c5fdffbe579bcddef72
Tomas Galeano <tom.ga.es@gmail.com>
2024-03-29 10:52:31 -0500

    Fixed height in vinculate form

--- src/components/vinculate.tsx
+++ src/components/vinculate.tsx
@@ -1,14 +1,39 @@
-
+import { useState, useEffect } from 'react';
 import "./Styles.css";
 import { Widget } from '@typeform/embed-react';
 
+function useWindowDimensions() {
+
+    const getWindowDimensions = () => {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+            width,
+            height: height * 0.83 // Get 75% of the window height
+        };
+    }
+
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowDimensions;
+}
+
 export const Vinculate = (): JSX.Element => {
+    const { height } = useWindowDimensions();
 
     return (
         <div className="form_container fifth">
             <Widget
                 id="r8CQhYfx" // Replace with your actual Typeform ID
-                height={750}
+                height={height}
             />
         </div>
     );
